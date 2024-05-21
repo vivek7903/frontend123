ecbc8a5a08d55701aef738b2ab4e5beeeaa7582d
Henryy <cute947122@gmail.com>
2024-05-21 11:52:02 +0530

    first commit

---
+++ project_myntra_clone/image/python workshop/hello.py
@@ -0,0 +1,33 @@
+print("hello world")
+num = 5
+name = "divya singh"
+adult = True
+marks = 90
+print(type(name))
+
+# num1= input("enter first number")
+# num2= input("enter second number") 
+# # print(num1 + num2)
+# sum = str(num1 + num2)
+# sub = str(num1 - num2)
+# mul = str(num1 * num2)
+# div = str(num1 / num2)
+
+# print(sum +"," +sub +","+ + mul + ","+ div)
+# marks = input("enter your marks")
+# if(marks >= 90):
+#     print("A") 
+# elif(marks >=60 and marks< 90):
+#     print("B")
+# else:
+#     print("F")
+
+i=1
+while(i<5):
+    print(i *  "*")
+    i = i + 1    
+
+j = 10
+while(j>1):
+    print(j* "*")
+    j = j -1
---
+++ project_myntra_clone/image/python workshop/tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+marks = input("enter your marks")
+# if(marks >= 90):
+#     print("A") 
+# elif(marks >=60 and marks< 90):
+#     print("B")
+# else:
+#     print("F")
